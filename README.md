566a4cc6870ecbfb75ecbcdb1db96c08274f30a7
ramsingh555 <97788602+ramsingh555@users.noreply.github.com>
2022-04-12 17:19:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+<b>Super Mario Game</b> - A clone of the classic mario game.
+<br>
+Created using html, css and javascript.
+<br>
+Live view at:- https://taruvar-mittal.github.io/super-mario-game/
---
+++ input.js
@@ -0,0 +1,54 @@
+
+let input = {
+    down: {},
+    pressed:{},
+    init() {
+       
+        window.addEventListener("keydown", (e) => {
+            this.down[e.code] = true;
+        })
+        window.addEventListener("keyup", (e) => {
+         delete this.down[e.code];
+         delete this.pressed[e.code];
+        })
+    }
+    ,
+    update(gameObj) {
+    let mario= gameObj.entities.mario
+
+    if(gameObj.userControl == true){
+      if(this.isDown("ArrowLeft")){
+        mario.posX-=mario.velX;
+        mario.currentDirection = "left";
+        mario.currentState=mario.states.walkingAnim;
+      }
+    
+      if(this.isDown("ArrowRight")){
+        mario.posX+=mario.velX;
+        mario.currentDirection = "right";
+        mario.currentState=mario.states.walkingAnim;
+      }
+    
+      if(this.isPressed("Space")){
+      if(mario.velY==1.1){
+        mario.velY-=14;
+        mario.currentState=mario.states.jumpingAnim;
+      }
+      }
+    }
+
+    }
+
+    ,isDown(key){
+        return this.down[key];
+    },
+    
+    isPressed(key){
+        if(this.pressed[key]){
+              return false;
+        }else if(this.down[key]){
+            return true;
+        }
+    }
+
+}
